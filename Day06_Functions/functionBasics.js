4c06549076095f551feb82f06cabcf8366a932c5
atharvakumare03 <157445719+atharvakumare03@users.noreply.github.com>
2024-09-27 22:52:46 +0530

    Create functionBasics.js

---
+++ Day06_Functions/functionBasics.js
@@ -0,0 +1,18 @@
+// Function without parameters
+
+function myFun()
+{
+    console.log("Hello Coders!!!");
+    console.log("We are learning JS");
+}
+
+myFun();
+
+// Function with parameters
+
+function myFun(a , b){
+
+    console.log(a+b);
+}
+
+myFun(10 , 20); // logs 30
